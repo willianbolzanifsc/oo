0c15086d6897b9e6cd4faa5799131b2916dc6748
Willian <willian.bolzan@ifsc.edu.br>
2022-07-28 22:07:17 -0300

    segundo commit

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,7 @@ public class Principal {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("dfdfd");
+            System.out.println(i);
 
         }
     }
